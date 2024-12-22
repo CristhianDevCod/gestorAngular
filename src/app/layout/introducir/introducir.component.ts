ab7c86f1b77768a39b092eabbba2cfed772a2b4c
Cristhian <cristhian19990302@gmail.com>
2024-12-22 11:52:00 -0500

    validación de campos

--- src/app/layout/introducir/introducir.component.ts
+++ src/app/layout/introducir/introducir.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { 
   FormsModule, 
   ReactiveFormsModule,
   FormControl, 
   FormGroup,
   Validators
-} from '@angular/forms'
-
-
+} from '@angular/forms';
 
 @Component({
   selector: 'app-introducir',
   standalone: true,
   imports: [
     FormsModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgClass
   ],
   templateUrl: './introducir.component.html',
   styles: ``
